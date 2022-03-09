20134770daebb7ab2313d98ed446d634becc0eb9
albertdc1984 <92040589+albertdc1984@users.noreply.github.com>
2022-03-09 19:14:12 +0100

    Created getAllUsers & getAllSessions controllers

---
+++ src/server/controllers/usersControllers/usersControllers.js
@@ -0,0 +1,10 @@
+const getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = getAllUsers;
