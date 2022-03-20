1e51cbfe34f5143404d4b6a80a9325fcc9269960
albertdc1984 <92040589+albertdc1984@users.noreply.github.com>
2022-03-20 22:49:09 +0100

    Created user login controller

--- src/server/controllers/usersControllers/usersControllers.js
+++ src/server/controllers/usersControllers/usersControllers.js
@@ -1,4 +1,6 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const debug = require("debug")("shrink:user:");
 const User = require("../../../database/models/User");
 
 const getAllUsers = async (req, res, next) => {
@@ -27,4 +29,30 @@ const createUser = async (req, res, next) => {
   }
 };
 
-module.exports = { getAllUsers, createUser };
+const userLogin = async (req, res, next) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) {
+    const error = new Error("User not found");
+    error.code = 401;
+    debug(error);
+    return next(error);
+  }
+
+  const rightPassword = await bcrypt.compare(password, user.password);
+
+  if (!rightPassword) {
+    const error = new Error("User not found");
+    error.code = 401;
+    debug(error);
+    return next(error);
+  }
+  // eslint-disable-next-line no-underscore-dangle
+  const userdata = { name: user.name, id: user._id };
+  const token = jwt.sign(userdata, process.env.JWT_SECRET, {
+    expiresIn: "4d",
+  });
+  return res.json({ token });
+};
+
+module.exports = { getAllUsers, createUser, userLogin };
