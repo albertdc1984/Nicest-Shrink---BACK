a6a2956b54f2ab22a80e0779ab49fd1917c1f9d8
albertdc1984 <92040589+albertdc1984@users.noreply.github.com>
2022-03-21 18:30:19 +0100

    Auth implemented on all routes except login & register

--- src/server/routes/router.js
+++ src/server/routes/router.js
@@ -14,17 +14,18 @@ const {
   getOneUser,
   deleteOneUser,
 } = require("../controllers/usersControllers/usersControllers");
+const auth = require("../middlewares/auth");
 
 const router = express.Router();
-router.get("/sessions", getAllSessions);
-router.get("/sessions/:id", getOneSession);
-router.delete("/sessions/:id", deleteOneSession);
-router.post("/sessions/new", createSession);
-router.patch("/sessions/:id", updateOneSession);
-router.get("/users", getAllUsers);
+router.get("/sessions", auth, getAllSessions);
+router.get("/sessions/:id", auth, getOneSession);
+router.delete("/sessions/:id", auth, deleteOneSession);
+router.post("/sessions/new", auth, createSession);
+router.patch("/sessions/:id", auth, updateOneSession);
+router.get("/users", auth, getAllUsers);
 router.get("/users/:id", getOneUser);
-router.delete("/users/:id", deleteOneUser);
+router.delete("/users/:id", auth, deleteOneUser);
 router.post("/users/new", createUser);
 router.post("/users/login", userLogin);
-router.patch("/users/:id", updateUser);
+router.patch("/users/:id", auth, updateUser);
 module.exports = router;
